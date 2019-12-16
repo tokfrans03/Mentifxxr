e93f0c4887b90be48109d578d57b261a936fcc12
tokfrans03 <34898241+tokfrans03@users.noreply.github.com>
2019-12-16 17:53:04 +0100

    ble

---
+++ quiz.py
@@ -0,0 +1,23 @@
+import Mentifxxr
+import requests
+
+pin = input("Input Pin\n> ")
+# pin = "342239"
+y = Mentifxxr.getInfo(pin)
+Mentifxxr.printInfo(y)
+
+t = Mentifxxr.getActiveQuestionType(y)
+q = Mentifxxr.getActiveQuestionid(y)
+
+i  = y["id"]
+
+newid = Mentifxxr.getNewID()
+
+headers = {
+    "x-identifier": newid,
+    "cookie": "identifier1=" + newid,
+    "Content-Type": "application/json",
+}
+
+a = requests.post(url = "https://www.menti.com/core/quiz/" + i + "/players/" + newid, headers=headers).text
+print(a)
